45a24bcc267b33f39de10981322aecfe0fdf7d29
SaqibFarhanProgrammer <saqibkhatai@gmail.com>
2026-06-03 21:04:01 +0500

    update ProjectCard link to include project name in URL

--- src/components/code/projects/ProjectCard.tsx
+++ src/components/code/projects/ProjectCard.tsx
@@ -19,7 +19,7 @@ const languageColors: Record<string, string> = {
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
-    <Link href={`/code/${project._id}`} className="block">
+    <Link href={`/code/${project.name}?id=${project._id}`} className="block">
       <div className="group relative p-5 rounded-xl bg-white/[0.03] border border-white/5 hover:border-[#0004ff]/30 hover:bg-white/[0.05] transition-all duration-300">
         <div className="absolute top-4 right-4">
           <span
