90e8f86c8fe52acab18a6e95976cbfac5d668f0c
SaqibFarhanProgrammer <saqibkhatai@gmail.com>
2026-06-13 22:09:12 +0500

    fixed ciritical sisssue

--- src/utils/AuthRequest.ts
+++ src/utils/AuthRequest.ts
@@ -2,35 +2,42 @@ import { NextRequest } from 'next/server';
 import jwt from 'jsonwebtoken';
 import { AppError } from '@/lib/AppError';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/dist/client/components/navigation';
+import { redirect } from 'next/navigation';
 
 export async function IsUserAuthenticate(req: NextRequest) {
   try {
     const token = req.cookies.get('token')?.value;
 
     if (!token) {
-      throw new AppError('unauthorize request please login', 400);
+      throw new AppError('Unauthorized', 401);
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET!
+    ) as {
       userId: string;
     };
 
     return decoded.userId;
-  } catch (error) {
+  } catch {
     return null;
   }
 }
 
 export async function GetUseridByToken() {
   const cookieStore = await cookies();
+
   const token = cookieStore.get('token')?.value;
 
   if (!token) {
     redirect('/auth/login');
   }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+  const decoded = jwt.verify(
+    token,
+    process.env.JWT_SECRET!
+  ) as {
     userId: string;
   };
 
